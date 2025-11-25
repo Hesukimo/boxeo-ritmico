6f84515992b4c3edb1d1dfe41387d13dcb7fff4f
Hesukimo <hesumo@alumnatflorida.es>
2025-11-25 09:58:05 +0100

    Arreglar Escena

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 public class Enemigo : MonoBehaviour
 {
     [SerializeField] int speed = 1;
+    private float lerpSpeed = 0.05f;
+    private Vector3 posObj;
     private int vida;
     private Transform jugador;
     private Vector2 direction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        posObj = transform.position;
         jugador = GameObject.Find("Jugador").GetComponent<Transform>();
         vida = 100;
         direction = new Vector2(jugador.position.x - transform.position.x, jugador.position.y - transform.position.y);
@@ -19,6 +22,11 @@ public class Enemigo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position != posObj)
+        {
+            transform.position = Vector3.Lerp(transform.position, posObj, lerpSpeed);
+        }
+
         if (Mathf.Abs(jugador.position.x - transform.position.x) < 1)
         {
             Destroy(this.gameObject);
@@ -27,6 +35,6 @@ public class Enemigo : MonoBehaviour
 
     public void Avanzar()
     {
-        transform.position += new Vector3(direction.x * speed, 0, 0);
+        posObj += new Vector3(direction.x * speed, 0, 0);
     }
 }
